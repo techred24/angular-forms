4ee20d48142317ab4b5f07ca61f88533377c61a3
TeChRF24 <f.rafael.temich24@gmail.com>
2023-02-12 02:38:45 -0600

    Creating new category

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { CategoriesService } from 'src/app/core/services/categories.service';
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -8,7 +10,9 @@ import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms'
 export class CategoryFormComponent implements OnInit {
   form: FormGroup;
   constructor(
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private categoriesService: CategoriesService,
+    private router: Router
   ) { 
     this.buildForm()
   }
@@ -26,6 +30,21 @@ export class CategoryFormComponent implements OnInit {
   }
   get imageField() {
     return this.form.get('image');
+  }
+  save () {
+    if (this.form.valid) {
+      this.createCategory();
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+  private createCategory() {
+    const data = this.form.value;
+    this.categoriesService.createCategory(data)
+      .subscribe(response => {
+        console.log(response);
+        this.router.navigate(['/admin/categories'])
+      })
   }
 
 }
