d8196ada188844547fff610a0c3945be024ae3a1
TeChRF24 <f.rafael.temich24@gmail.com>
2023-02-13 01:05:36 -0600

    Trying to get an url from firebase. Connection unsuccessful

--- src/app/admin/categories/components/category-form/category-form.component.ts
+++ src/app/admin/categories/components/category-form/category-form.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormControl, Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { CategoriesService } from 'src/app/core/services/categories.service';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
+// import { Storage, ref, uploadBytes, listAll, getDownloadURL, StorageReference } from '@angular/fire/storage';
+import { finalize } from 'rxjs/operators';
+
 @Component({
   selector: 'app-category-form',
   templateUrl: './category-form.component.html',
@@ -12,7 +16,9 @@ export class CategoryFormComponent implements OnInit {
   constructor(
     private formBuilder: FormBuilder,
     private categoriesService: CategoriesService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
+    // private storage: Storage
   ) { 
     this.buildForm()
   }
@@ -46,5 +52,54 @@ export class CategoryFormComponent implements OnInit {
         this.router.navigate(['/admin/categories'])
       })
   }
+  uploadFile(event) {
+    const image = event.target.files[0];
+    const name = 'category.png';
+    const ref = this.storage.ref(name);
+    const task = this.storage.upload(name, image);
+
+    task.snapshotChanges()
+    .pipe(
+      finalize(() => {
+        const urlImage$ = ref.getDownloadURL();
+        urlImage$.subscribe((url) => {
+          console.log(url);
+          this.imageField.setValue(url);
+        })
+      })
+    )
+    .subscribe()
+  }
+
+  // uploadFile(event: any) {
+  //   const image = event.target.files[0];
+  //   const name = image.name;
+
+  //   const imgRef = ref(this.storage,`imagenes/${name}`);
+  //   const task = uploadBytes(imgRef,image);
+
+  //   task
+  //     .then(response => {
+  //       console.log(response);
+  //       this.getImage(name)
+  //     })
+  //     .catch(error => console.log(error))
+  // }
+
+  // getImage(nameImage:string) {
+  //   const imgRef = ref(this.storage, 'imagenes')
+  //   listAll(imgRef)
+  //     .then( async rta => {
+
+  //       const itemActual: StorageReference|undefined = rta.items.find(item => item.name === nameImage);
+
+  //       if(itemActual){
+  //         const url = await getDownloadURL(itemActual)
+  //         this.imageField?.setValue(url);
+  //         console.log(url);
+  //       }
+
+  //     })
+  // }
 
 }
